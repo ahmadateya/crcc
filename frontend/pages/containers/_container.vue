ea6aab742cfa530302466c3426958c5376103e64
Ahmad Ateya <ahmad.m.ateya@gmail.com>
2022-04-26 15:44:02 +0200

    init scan part

--- frontend/pages/containers/_container.vue
+++ frontend/pages/containers/_container.vue
@@ -3,7 +3,10 @@
     <base-header class="pb-6">
     </base-header>
     <div class="container-fluid mt--6">
+      <!--   Container Table   -->
       <div class="row">
+        <h2 v-if="loaded.error">Error while fetching data please request it again.</h2>
+        <h2 v-else-if="loaded.responseError">Please make sure of allowing the Rest API.</h2>
         <div class="col-xl-12">
           <div class="border-0 card-header">
             <div class="row">
@@ -11,12 +14,17 @@
                 <h1 class="mb-0 bold ">{{container.name}} </h1>
               </div>
               <div class="col-xl-2">
-                <base-button size="lg">Scan</base-button>
+                <base-button size="lg" @click="scanContainer">Scan</base-button>
              </div>
             </div>
           </div>
           <view-container :container="container"/>
         </div>
+      </div>
+
+      <!--  Scan Part    -->
+      <div class="col-xl-12">
+        <h2 v-if="isScanned">Scanned</h2>
       </div>
     </div>
   </div>
@@ -38,7 +46,9 @@ export default {
     return {
       container: {},
       loaded:{},
-      valid:true
+      valid:true,
+      isScanned:false,
+      scanData: {}
     }
   },
   async fetch() {
@@ -53,5 +63,22 @@ export default {
           this.loaded.error="Error while requesting data please try again."
         });
   },
+  methods: {
+    async scanContainer() {
+      this.isScanned = true;
+
+      await this.$axios.get(`${url}/containers/${this.$route.params.container}/scan`)
+        .then(response => {
+          if(response.status!==200){
+            this.loaded.responseError=true;
+            return;
+          }
+          this.scanData = response.data;
+          this.isScanned = true;
+        }).catch(err=> {
+            // this.isScanned = false;
+            this.loaded.error="Error while requesting data please try again."});
+    }
+  }
 };
 </script>
