3ad3ff065e3559213897e600a14f3663e205c4ff
Ahmad Ateya <ahmad.m.ateya@gmail.com>
2022-05-08 06:40:10 +0200

    project title

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -44,7 +44,7 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: "Nuxt Argon Dashboard Laravel by Creative Tim & UPDIVISION",
+    title: "Container Runtime Compliance Checker",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
