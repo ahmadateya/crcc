0bf08f7176ef2ee5452d7cae1a41d312b50b52de
Ahmad Ateya <ahmad.m.ateya@gmail.com>
2022-05-27 14:02:52 +0200

    history page done

--- frontend/pages/containers/_container/history/index.vue
+++ frontend/pages/containers/_container/history/index.vue
@@ -34,72 +34,92 @@
           </div>
         </div>
       </div>
-      <br><br>
+      <br>
       <!--  History Part    -->
       <div v-for="(scan, scanIndex) in history" class="col-xl-12">
+        <div class="border card-header ">
+          <div class="row">
+            <div class="col-xl-10">
+              <h1 class="mb-0 bold ">
+                <i class="ni ni-calendar-grid-58"></i> <span> {{ scan.CreatedAt }}</span>
+              </h1>
+            </div>
+          </div>
+        </div>
         <div class="border card-body ">
-        <div class="row">
-          <!-- Analysis Part -->
-          <div class="col-xl-6 col-md-6">
-            <h1>
-              <i class="ni ni-chart-bar-32"></i> <span> {{ scan.CreatedAt }}</span>
-            </h1>
-            <!-- Via multiple directive modifiers -->
-            {{ }}
-            <div class="element"
-                 v-for="(result, index) in JSON.parse(scan.scan).results"
-            >
-              <b-button v-b-toggle="'collapse-' + scanIndex + '-' + index + '-details'"
-                        :id="'collapse-' + scanIndex + '-' + index "
-                        class="align-left w-75 p-3 mb-1 "
-                        v-bind:class="[result.passed ? 'text-success' : 'passed text-danger' ]"
-              >
-                <i v-if="result.passed" class="ni ni-check-bold"></i>
-                <i v-else class="ni ni-fat-remove"></i>
-                {{ result.title }}
-              </b-button>
-              <div v-if="result.details !== ''">
-                <b-collapse :id="'collapse-' + scanIndex + '-' + index + '-details'" class="mt-2">
-                  <!--    details is an array of objects  -->
-                  <b-card-text v-if="result.details.charAt(0) === '['">
-                    <ol>
-                      <li v-for="detail in JSON.parse(result.details)">
-                        <br>
-                        <ul>
-                          <li v-for="(value, key) in detail">
-                            <b>{{ key }} : </b> <span>{{ value }}</span>
-                          </li>
-                        </ul>
-                      </li>
-                    </ol>
-                  </b-card-text>
-                  <!--      details is a string of data -->
-                  <b-card-text v-else>
-                    {{ result.details }}
-                  </b-card-text>
-                </b-collapse>
+          <div class="row">
+            <!-- Analysis Part -->
+            <div class="col-xl-12 col-md-12">
+              <!-- Via multiple directive modifiers -->
+              <div class="row">
+                <div class="col-xl-6 col-md-6">
+                  <h2>
+                    <i class="ni ni-chart-bar-32"></i> <span> Scan Results</span>
+                  </h2>
+                  <div class="element"
+                       v-for="(result, index) in JSON.parse(scan.scan).results"
+                  >
+                    <b-button v-b-toggle="'collapse-' + scanIndex + '-' + index + '-details'"
+                              :id="'collapse-' + scanIndex + '-' + index "
+                              class="align-left w-75 p-3 mb-1 "
+                              v-bind:class="[result.passed ? 'text-success' : 'passed text-danger' ]"
+                    >
+                      <i v-if="result.passed" class="ni ni-check-bold"></i>
+                      <i v-else class="ni ni-fat-remove"></i>
+                      {{ result.title }}
+                    </b-button>
+                    <div v-if="result.details !== ''">
+                      <b-collapse :id="'collapse-' + scanIndex + '-' + index + '-details'" class="mt-2">
+                        <!--    details is an array of objects  -->
+                        <b-card-text v-if="result.details.charAt(0) === '['">
+                          <ol>
+                            <li v-for="detail in JSON.parse(result.details)">
+                              <br>
+                              <ul>
+                                <li v-for="(value, key) in detail">
+                                  <b>{{ key }} : </b> <span>{{ value }}</span>
+                                </li>
+                              </ul>
+                            </li>
+                          </ol>
+                        </b-card-text>
+                        <!--      details is a string of data -->
+                        <b-card-text v-else>
+                          {{ result.details }}
+                        </b-card-text>
+                      </b-collapse>
+                    </div>
+                  </div>
+                </div>
+                <!--  Chart Part  -->
+                <div class="col-xl-6 col-md-6">
+                  <h2>
+                    <i class="ni ni-chart-pie-35"></i>
+                    <span> Compliance Percentage</span>
+                    <span
+                        v-bind:class="[JSON.parse(scan.scan).compliance >= 50 ? 'text-success' : 'passed text-danger' ]"
+                    >{{ JSON.parse(scan.scan).compliance }} %</span>
+                  </h2>
+                  <pie-chart
+                      :height="200"
+                      ref="pieChart"
+                      :chart-data="{
+                          labels: ['Failed', 'Passed'],
+                          datasets: [
+                            {
+                              backgroundColor: ['#DD1B16', '#41B883'],
+                              data: [100 - JSON.parse(scan.scan).compliance, JSON.parse(scan.scan).compliance],
+                            },
+                          ],
+                        }"
+                  >
+                  </pie-chart>
+                </div>
               </div>
             </div>
           </div>
-          <!--  Chart Part  -->
-          <div class="col-xl-6 col-md-6">
-            <h1>
-              <i class="ni ni-chart-pie-35"></i>
-              <span> Compliance Percentage</span>
-              <span
-                  v-bind:class="[scan.result >= 50 ? 'text-success' : 'passed text-danger' ]"
-              >{{scan.result}} %</span>
-            </h1>
-            <!--            <pie-chart-->
-            <!--                        v-if="isScanned"-->
-            <!--                       :height="250"-->
-            <!--                       ref="pieChart"-->
-            <!--                       :chart-data="chartData"-->
-            <!--            >-->
-            <!--            </pie-chart>-->
-          </div>
         </div>
-        </div>
+        <br>
       </div>
     </div>
   </div>
@@ -145,37 +165,6 @@ export default {
           this.loaded.error = "Error while requesting data please try again."
         });
   },
-  methods: {
-    async scanContainer() {
-      this.isScanned = true;
-      this.$nuxt.$loading.start()
-
-      await this.$axios.get(`${url}/containers/${this.$route.params.container}/scan`)
-          .then(response => {
-            if (response.status !== 200) {
-              this.loaded.responseError = true;
-              return;
-            }
-            this.scanData = response.data;
-            this.chartData = {
-              labels: ['Failed', 'Passed'],
-              datasets: [
-                {
-                  backgroundColor: ['#DD1B16', '#41B883'],
-                  data: [100 - this.scanData.compliance, this.scanData.compliance],
-                },
-              ],
-            };
-            this.isScanned = true;
-            this.$nuxt.$loading.finish()
-          }).catch((err) => {
-            this.$nuxt.$loading.finish()
-            this.isScanned = false;
-            this.loaded.error = "Error while requesting data please try again.";
-            console.log(err, "error");
-          });
-    }
-  }
 };
 </script>
 <style scoped>
@@ -190,6 +179,4 @@ export default {
   border-color: #2496ed;
 }
 
-.card-header {
-}
 </style>
